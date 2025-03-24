b31ff5fb6a4003ff570715037c204147a36db79f
Mikalai Kisel <kisel.nikolay@gmail.com>
2025-03-24 14:51:23 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,3 @@ variable "internet_cidr" {
   type        = string
   default     = "0.0.0.0/0"
 }
-
-variable "local_network_cidr" {
-  description = "CIDR block for the local network"
-  type        = string
-  default     = "10.0.0.0/16"  # Adjust the default value to match your local network
-}
