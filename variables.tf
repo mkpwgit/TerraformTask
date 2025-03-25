93800cc073e1d7f579c0649f6cd965c3f8964a54
Mikalai Kisel <kisel.nikolay@gmail.com>
2025-03-25 14:09:56 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,21 @@ variable "internet_cidr" {
   type        = string
   default     = "0.0.0.0/0"
 }
+
+variable "local_network_cidr" {
+  description = "CIDR block for the local network"
+  type        = string
+  default     = "10.0.0.0/16"  # Adjust the default value to match your local network
+}
+
+variable "subnet1_cidr" {
+  description = "CIDR block for the subnet1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet2_cidr" {
+    description = "CIDR block for the subnet2"
+    type        = string
+    default     = "10.0.2.0/24"
+}
